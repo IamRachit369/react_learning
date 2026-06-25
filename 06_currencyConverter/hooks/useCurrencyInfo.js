49deeac203893bf24bd73f1a66ba1364efb47060
Rachit Saini <rachit.college.mnnit@gmail.com>
2026-06-25 20:25:44 +0530

    making reusable components

--- 06_currencyConverter/hooks/useCurrencyInfo.js
+++ 06_currencyConverter/hooks/useCurrencyInfo.js
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
+const [currrency, setcurrrency] = useState(second)
+function fetchData()
+{
+    const [data, setdata] = useState({})
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`).then((res)=>{res.json()}).then((res)=>{setdata(res[currrency])})
+    console.log(data)
+    return data
+}
 
 function useCurrencyInfo(currency)
 {
-    useEffect(()=>{
-        fetch()
-    }, [])
+    useEffect(()=>{fetchData}, [currency])
 }
+export default useCurrencyInfo 
